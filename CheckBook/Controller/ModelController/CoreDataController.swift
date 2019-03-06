5fe71b5a39ac7f1fd1e25c85c35de88c955b3852
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-06 11:54:01 -0700

    Adds a read function

--- CheckBook/Controller/ModelController/CoreDataController.swift
+++ CheckBook/Controller/ModelController/CoreDataController.swift
@@ -15,7 +15,27 @@ class CoreDataController {
     /// The shared Instance of CoreDataController.
     static let shared = CoreDataController()
     
-    //MARK: - FetchResultsController
+    //MARK: Read
+    /// Looks in the Context for a Purchase with a given UUID.
+    /// - parameter uuid: The uuid of the Puchease that is being searched for.
+    /// - parameter completion: Handler for when the purchase has been found.
+    /// - parameter foundPurchase: The purchase that was found or nil.
+    func findPurchaseWith(uuid: UUID?, completion: @escaping (_ foundPurchase:Purchase?) -> Void ) {
+        guard let uuid = uuid else {
+            completion(nil)
+            return
+        }
+        let request: NSFetchRequest<Purchase> = Purchase.fetchRequest()
+        request.fetchLimit = 1
+        request.predicate = NSPredicate(format: "%K == %@", #keyPath(Purchase.uuid), uuid as CVarArg)
+        do {
+            let purchases = try CoreDataStack.context.fetch(request)
+            completion(purchases.first)
+        } catch {
+            print("No Purchase with UUID fouund: \(error), \(error.localizedDescription)")
+            completion(nil)
+        }
+    }
     
     //MARK: - Delete
     /// Removes the Purchase from the Context.
---
+++ CheckBook/Controller/ModelController/SyncController.swift
@@ -0,0 +1,24 @@
+//
+//  SyncController.swift
+//  CheckBook
+//
+//  Created by Dominic Lanzillotta on 3/6/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import CoreData
+
+class SyncController {
+    
+    //MARK: - Singleton
+    /// The shared Instance of SyncController.
+    static let shared = SyncController()
+    
+    //compare both Stores(uuid and lastUpdated)
+    //CoreData has update -> notify CloudKit
+    //CloudKit has update -> notify CoreData
+    
+    //if CK not available
+}
