81225051b9bd98573d067ce4c0da151799ffb503
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-03-05 17:02:59 -0700

    Adds create: and update:

---
+++ CheckBook/Controller/ModelController/PurchaseController.swift
@@ -0,0 +1,62 @@
+//
+//  PurchaseController.swift
+//  CheckBook
+//
+//  Created by Dominic Lanzillotta on 3/5/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+
+
+class PurchaseController {
+    //MARK: - Singleton
+    /// The shared Instance of ContactController.
+    static let shared = PurchaseController()
+    
+    //MARK: - Properties
+    /// The private Database of the User.
+    fileprivate let privateDB = CKContainer.default().privateCloudDatabase
+    
+    //MARK: - CRUD
+    /// Creates new Purchase.
+    /// - parameter amount: The amount of the purchase.
+    /// - parameter date: The date of the purchase.
+    /// - parameter item: The itemName of the purchase.
+    /// - parameter storeName: The storeName of the purchase.
+    /// - parameter method: The method of payment of the purchase.
+    func createNewPurchaseWith(amount: Double, date: Date, item: String, storeName:String, method: String) {
+        let newPurchase = Purchase(amount: amount, date: date, item: item , storeName: storeName, method: method)
+        save(purchase: newPurchase)
+    }
+    
+    /// Updates the Purchase.
+    /// - parameter purchase: The purchase to update.
+    /// - parameter amount: The updated amount of the purchase.
+    /// - parameter date: The updated date of the purchase.
+    /// - parameter item: The updated itemName of the purchase.
+    /// - parameter storeName: updated The storeName of the purchase.
+    /// - parameter method: The updated method of payment of the purchase.
+    func update(purchase: Purchase, amount:Double?, date: Date?, item: String?, storeName: String?, method: String?) {
+        if let amount = amount {purchase.amount = amount}
+        if let date = date {purchase.date = date}
+        if let item = item {purchase.item = item}
+        if let storeName = storeName {purchase.storeName = storeName}
+        if let method = method { purchase.method = method}
+        purchase.lastModified = Date()
+        save(purchase: purchase)
+    }
+    
+    /// Deletes the Purchase.
+    /// - parameter purchase: The purchase to delete.
+    func delete(purchase: Purchase) {
+        
+    }
+    
+    //MARK: - Save
+    /// - parameter purchase: The purchase to save.
+    private func save(purchase: Purchase) {
+        
+    }
+}
