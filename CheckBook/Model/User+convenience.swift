de580b518ba42d9f944672973a9fb3e116258f7d
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-04-08 12:30:43 -0600

    Adds User Model

---
+++ CheckBook/Model/User+convenience.swift
@@ -0,0 +1,58 @@
+//
+//  User+convenience.swift
+//  CheckBook
+//
+//  Created by Dominic Lanzillotta on 4/8/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import CloudKit
+
+extension User {
+    @discardableResult
+    convenience init(name: String,
+                     color: String? = nil,
+                     appleUserUUID: UUID,
+                     uuid: UUID = UUID(),
+                     purchases: NSOrderedSet = NSOrderedSet(),
+                     lastModified: Date = Date(),
+                     context: NSManagedObjectContext = CoreDataStack.context) {
+        
+        self.init(context: context)
+        
+        self.name = name
+        self.appleUserUUID = appleUserUUID
+        self.uuid = uuid
+        self.purchases = purchases
+        self.color = color
+        
+        self.lastModified = lastModified
+    }
+    
+    convenience init?(record: CKRecord, context: NSManagedObjectContext = CoreDataStack.context) {
+        guard let name = record[User.nameKey] as? String,
+            let color = record[User.colorKey] as? String,
+            let appleUserUUID = record[User.appleUserKey] as? String,
+            let lastModified = record[User.lastModifiedKey] as? Date else {return nil}
+        
+        self.init(name: name, color: color, appleUserUUID: UUID(uuidString: appleUserUUID)!, uuid: UUID(uuidString: record.recordID.recordName)!, lastModified: lastModified, context: context)
+    }
+}
+
+extension CKRecord {
+    convenience init?(user: User) {
+        self.init(recordType: User.typeKey, recordID: CKRecord.ID(recordName: user.uuid!.uuidString, zoneID: CKRecordZone.ID(zoneName: Purchase.privateRecordZoneName, ownerName: CKCurrentUserDefaultName)))
+        
+        let appleUserRef = CKRecord.Reference(recordID: CKRecord.ID(recordName: user.appleUserUUID!.uuidString, zoneID: CKRecordZone.ID(zoneName: Purchase.privateRecordZoneName, ownerName: CKCurrentUserDefaultName)) , action: .deleteSelf)
+        
+        guard let appleUserUUID = user.appleUserUUID else {return nil}
+        
+        setValue(user.name, forKey: User.nameKey)
+        setValue(user.color, forKey: User.colorKey)
+        setValue(user.lastModified, forKey: User.lastModifiedKey)
+        setValue(appleUserUUID.uuidString, forKey: User.appleUserKey)
+        setValue(appleUserRef, forKey: User.appleUserReferenceKey)
+    }
+}
