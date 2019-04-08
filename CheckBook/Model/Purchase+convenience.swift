c8f3c0a24e10d58652428f63a3e4f150c9054c4d
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-04-08 15:25:20 -0600

    Extends Purchase conveninece functions with User Model

--- CheckBook/Model/Purchase+convenience.swift
+++ CheckBook/Model/Purchase+convenience.swift
@@ -20,6 +20,7 @@ extension Purchase {
                      lastModified: Date = Date(),
                      purchaseMethod: PurchaseMethod,
                      category: Category,
+                     user: User,
                      ledger: Ledger,
                      context: NSManagedObjectContext = CoreDataStack.context) {
         
@@ -32,68 +33,17 @@ extension Purchase {
         self.purchaseMethod = purchaseMethod
         self.ledger = ledger
         self.category = category
+        self.user = user
         
         self.methodName = purchaseMethod.name
         self.methodUUID = purchaseMethod.uuid
         self.ledgerUUID = ledger.uuid
         self.categoryUUID = category.uuid
+        self.userUUID = user.uuid
+        self.appleUserUUID = user.appleUserUUID
         
         self.lastModified = lastModified
         self.uuid = uuid
-    }
-    
-    convenience init?(record: CKRecord, context: NSManagedObjectContext = CoreDataStack.context) {
-        guard let amount = record[Purchase.amountKey] as? NSDecimalNumber,
-            let date = record[Purchase.dateKey] as? Date,
-            let item = record[Purchase.itemKey] as? String,
-            let methodUUID = record[Purchase.methodKey] as? String,
-            let ledgerUUID = record[Purchase.ledgerKey] as? String,
-            let categoryUUID = record[Purchase.categoryKey] as? String,
-            let methodName = record[Purchase.methodNameKey] as? String,
-            let lastModified = record[Purchase.lastModifiedKey] as? Date,
-            let storeName = record[Purchase.storeNameKey] as? String else {return nil}
-        
-        
-        self.init(context: context)
-        
-        CoreDataController.shared.findPurchaseMethodWith(uuid: UUID(uuidString: methodUUID)!) { [weak self] (foundPurchaseMethod) in
-            if let foundPurchaseMethod = foundPurchaseMethod {
-                self?.purchaseMethod  = foundPurchaseMethod
-            } else {
-                self?.purchaseMethod = PurchaseMethod(name: methodName, uuid: UUID(uuidString: methodUUID)!)
-            }
-        }
-        
-        CoreDataController.shared.findLedgerWith(uuid: UUID(uuidString: ledgerUUID)!) { [weak self] (foundLedger) in
-            if let foundLedger = foundLedger {
-                self?.ledger  = foundLedger
-            } else {
-                self?.ledger = Ledger(name: "", uuid: UUID(uuidString: ledgerUUID)!)
-            }
-        }
-        
-        
-        CoreDataController.shared.findCategoryWith(uuid: UUID(uuidString: categoryUUID)!) { [weak self](foundCategory) in
-            if let foundCategory = foundCategory {
-                self?.category = foundCategory
-            } else {
-                self?.category = Category(name: "", uuid: UUID(uuidString: categoryUUID)!)
-            }
-        }
-        
-        self.amount = amount
-        self.date = date
-        self.item = item
-        self.storeName = storeName
-        
-        self.ledgerUUID = UUID(uuidString: ledgerUUID)!
-        self.methodName = methodName
-        self.methodUUID = UUID(uuidString: methodUUID)!
-        self.categoryUUID = UUID(uuidString: categoryUUID)!
-        
-        
-        self.lastModified = lastModified
-        self.uuid = UUID(uuidString: record.recordID.recordName)!
     }
 }
 
@@ -106,18 +56,23 @@ extension CKRecord {
         let ledgerReference = CKRecord.Reference(recordID: CKRecord.ID(recordName: purchase.ledgerUUID!.uuidString, zoneID: CKRecordZone.ID(zoneName: Purchase.privateRecordZoneName, ownerName: CKCurrentUserDefaultName)), action: .none)
         
         let categoryReference = CKRecord.Reference(recordID: CKRecord.ID(recordName: purchase.categoryUUID!.uuidString, zoneID: CKRecordZone.ID(zoneName: Purchase.privateRecordZoneName, ownerName: CKCurrentUserDefaultName)), action: .none)
+        
+        let userReference = CKRecord.Reference(recordID: CKRecord.ID(recordName: purchase.userUUID!.uuidString), action: .none)
 
-        guard let methodUUID = purchase.purchaseMethod?.uuid, let methodName = purchase.purchaseMethod?.name, let ledgerUUID = purchase.ledger?.uuid, let categoryUUID = purchase.category?.uuid else {return nil}
+        guard let methodUUID = purchase.purchaseMethod?.uuid, let methodName = purchase.purchaseMethod?.name, let ledgerUUID = purchase.ledger?.uuid, let categoryUUID = purchase.category?.uuid, let appleUserUUID = purchase.user?.appleUserUUID, let userUUID = purchase.user?.uuid else {return nil}
         
         setValue(purchase.amount, forKey: Purchase.amountKey)
         setValue(purchaseMethodReference, forKey: Purchase.methodReferenceKey)
         setValue(ledgerReference, forKey: Purchase.ledgerReferenceKey)
         setValue(categoryReference, forKey: Purchase.categoryReferenceKey)
+        setValue(userReference, forKey: Purchase.userReferenceKey)
         setValue(purchase.date, forKey: Purchase.dateKey)
         setValue(purchase.item, forKey: Purchase.itemKey)
         setValue(methodUUID.uuidString, forKey: Purchase.methodKey)
         setValue(ledgerUUID.uuidString, forKey: Purchase.ledgerKey)
         setValue(categoryUUID.uuidString, forKey: Purchase.categoryKey)
+        setValue(appleUserUUID.uuidString, forKey: Purchase.appleUserKey)
+        setValue(userUUID.uuidString, forKey: Purchase.userKey)
         setValue(methodName, forKey: Purchase.methodNameKey)
         setValue(purchase.lastModified, forKey: Purchase.lastModifiedKey)
         setValue(purchase.storeName, forKey: Purchase.storeNameKey)
