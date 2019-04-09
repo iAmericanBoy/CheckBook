8e17accb2c201b2169d8f16da08d65c7d57764f8
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-04-09 15:37:45 -0600

    Adds Setting Enum to populate SettingsVC

--- CheckBook/Controller/ViewController/SettingsViewController.swift
+++ CheckBook/Controller/ViewController/SettingsViewController.swift
@@ -11,17 +11,41 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     //MARK: - Outlets
-
+    @IBOutlet weak var tableView: UITableView!
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
 
     }
     //MARK: - Actions
-    
     @IBAction func backButtonTapped(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
-    //MARK: - Private Functions
+}
+
+//MARK: - UITableViewDataSource, UITableViewDelegate
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Setting.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as? SettingsTableViewCell
+        cell?.setting = Setting.allCases[indexPath.row]
+        cell?.delegate = self
+        return cell ?? UITableViewCell()
+    }
+}
+
+//MARK: - SettingsDelegate
+extension SettingsViewController: SettingsDelegate {
+    func shareLedger() {
+        print("SHARE!")
+    }
+    
+    
 }
